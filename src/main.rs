0ecfa32f765bbe50515102cc7d0d27be160601d3
Thomas <git@thomas.klierlinge.de>
2020-12-02 22:27:39 +0100

    Fill text in window with content

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ extern crate gtk;
 extern crate gio;
 
 use gtk::prelude::*;
-use gio::prelude::*;
 use std::env;
-use gtk::{Application, ApplicationWindow, Button, Builder, Window};
+use gtk::*;
 use std::process::Command;
 use gio::ApplicationExt;
 use gio::prelude::ApplicationExtManual;
@@ -19,17 +18,26 @@ fn main() {
         let glade_src = include_str!("main_window.glade");
         let builder = Builder::from_string(glade_src);
         let window: Window = builder.get_object("main_window").expect("Couldn't get window");
+        let screen = window.get_screen().unwrap();
+        let text: TextView = builder.get_object("test_text").expect("Couldn't get test_text");
+
+        let style: CssProvider = CssProvider::new();
+        let css = include_str!("style.css");
+        style.load_from_data(&css.as_bytes()).expect("Load style failed");
+        StyleContext::add_provider_for_screen(&screen, &style, STYLE_PROVIDER_PRIORITY_USER);
+
         window.set_application(Some(app));
 
+        let output = Command::new("ls")
+            .arg("-halF")
+            .arg(env::var("HOME").unwrap_or(".".to_string()))
+            .output()
+            .expect("failed to execute process");
+        text.get_buffer()
+            .expect("Couldn't get window")
+            .set_text(&String::from_utf8_lossy(&output.stdout));
         window.show_all();
     });
 
     application.run(&[]);
-
-    let output = Command::new("ls")
-        .arg("-halF")
-        .arg(env::var("HOME").unwrap_or(".".to_string()))
-        .output()
-        .expect("failed to execute process");
-    println!("{}", String::from_utf8_lossy(&output.stdout));
 }
