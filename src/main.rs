41222eafc832e45b3bc6db34de692e9ebd510e7c
Thomas <git@thomas.klierlinge.de>
2021-08-07 18:24:09 +0200

    Show each btrfs drive in one tab

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 extern crate gtk;
 extern crate gio;
 
-use gtk::prelude::*;
 use std::env;
-use gtk::*;
-use std::process::Command;
 use gio::ApplicationExt;
 use gio::prelude::ApplicationExtManual;
+use gtk::prelude::*;
+use gtk::*;
+use std::path::Path;
+use std::process::Command;
 
 fn main() {
+    env::set_var("LC_ALL", "en_US.UTF-8");
     let application = Application::new(
-        Some("com.github.gtk-rs.examples.basic"),
+        Some("com.github.Mr-Clear.btrfsgui"),
         Default::default(),
     ).expect("failed to initialize GTK application");
 
@@ -19,7 +21,8 @@ fn main() {
         let builder = Builder::from_string(glade_src);
         let window: Window = builder.get_object("main_window").expect("Couldn't get window");
         let screen = window.get_screen().unwrap();
-        let text: TextView = builder.get_object("test_text").expect("Couldn't get test_text");
+        let drives_notebook: Notebook = builder.get_object("drives").expect("Couldn't get drives Notebook");
+
 
         let style: CssProvider = CssProvider::new();
         let css = include_str!("style.css");
@@ -28,16 +31,37 @@ fn main() {
 
         window.set_application(Some(app));
 
-        let output = Command::new("ls")
-            .arg("-halF")
-            .arg(env::var("HOME").unwrap_or(".".to_string()))
-            .output()
-            .expect("failed to execute process");
-        text.get_buffer()
-            .expect("Couldn't get window")
-            .set_text(&String::from_utf8_lossy(&output.stdout));
+        let drives = get_btrfs_drives();
+        for drive in drives {
+            println!("{}", drive);
+            let l1 = Label::new(Some(drive.as_str()));
+            let l2 = Label::new(Some(drive.as_str()));
+            drives_notebook.append_page(&l1, Some(&l2));
+        }
+
         window.show_all();
     });
 
     application.run(&[]);
+}
+
+fn get_btrfs_drives() -> Vec<String> {
+    let output = Command::new("mount")
+        .output().expect("failed to execute 'mount'");
+    let text = std::str::from_utf8(&output.stdout).expect("Output of mount is no valid utf-8");
+    let lines = text.split("\n");
+    let mut drives: Vec<&str> = Vec::new();
+    for line in lines {
+        let fields: Vec<&str> = line.split(" ").collect();
+        if fields.len() > 4 && fields[4] == "btrfs" {
+            let drive = fields[2];
+            if Path::new(drive).is_dir() {
+                drives.push(drive);
+            }
+            else {
+                eprintln!("Mount is no valid directory: {}", drive);
+            }
+        }
+    }
+    return drives.iter().map(|s| s.to_string()).collect();
 }
